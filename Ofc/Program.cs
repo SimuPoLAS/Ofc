eeab294e3fa5abadd02d99181fe8144e63230b98
Patrick Widauer <a__ctor@hotmail.com>
2016-04-04 18:06:27 +0200

    Removed testing things from the Program.cs

--- Ofc/Program.cs
+++ Ofc/Program.cs
@@ -1,47 +1,12 @@
 ﻿namespace Ofc
 {
     using System;
-    using System.Collections;
-    using System.IO;
-    using System.Linq;
     using Ofc.CommandLine;
-    using Ofc.IO;
-    using Ofc.Parsing;
-    using Ofc.Parsing.Hooks;
-    using Ofc.Util;
 
     internal class Program
     {
         public static void Main(string[] args)
         {
-
-            using (var memory = new MemoryStream())
-            {
-                using (var writer = new BinaryDataWriter(memory))
-                {
-                    writer.WriteId(true, 7, 0);
-                }
-
-                Console.WriteLine($"[{string.Join(", ", BitConverter.GetBytes(2))}]");
-
-                Console.WriteLine($"l: {memory.Length}");
-                memory.Position = 0;
-                using (var reader = new BinaryDataReader(memory))
-                {
-                    
-                }
-            }
-
-            return;
-
-
-            Console.SetOut(File.CreateText("W:/desktop/z/log"));
-            var lexer = new OfcLexer(new FileInputStream("W:/desktop/z/p"), false);
-            var parser = new OfcParser(lexer, new DebugHook<string>());
-            parser.Parse();
-
-            return;
-
             // initiate the parser
             IArgumentParser<CommandLineLayers> argumentParser = new ArgumentParser<CommandLineLayers>();
             argumentParser.Description = "A command line tool for compressing Open Foam files.";
