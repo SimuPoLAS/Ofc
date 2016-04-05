592c589fb5283b0fcf77737f42ef7ac839ea5d91
Patrick Widauer <a__ctor@hotmail.com>
2016-04-05 20:21:29 +0200

    Documented the EmptyConfigurations class.

--- OfcCore/Configurations/EmptyConfiguration.cs
+++ OfcCore/Configurations/EmptyConfiguration.cs
@@ -2,22 +2,37 @@
 {
     using JetBrains.Annotations;
 
+    /// <summary>
+    ///     Represents an empty configuration. It can not be filled.
+    /// </summary>
     public class EmptyConfiguration : IConfiguaration
     {
+        /// <summary>
+        ///     Private singleton member.
+        /// </summary>
         private static IConfiguaration instance;
 
+        /// <summary>
+        ///     Singlton instance of <see cref="EmptyConfiguration" />.
+        /// </summary>
         public static IConfiguaration Instance => instance ?? (instance = new EmptyConfiguration());
 
 
+        /// <summary>
+        ///     Private constructor for singleton use only.
+        /// </summary>
         private EmptyConfiguration()
         {
         }
 
 
+        [ContractAnnotation("=> null")]
         public object Get(string name) => null;
 
+        [ContractAnnotation("=> false")]
         public bool Has(string name) => false;
 
+        [ContractAnnotation("=> false")]
         public bool Remove(string name) => false;
 
         public void Set(string name, [CanBeNull] object value)
