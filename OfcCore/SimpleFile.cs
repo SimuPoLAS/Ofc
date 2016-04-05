9219ff8714669c83aab96ef97572a46e5db61d96
Patrick Widauer <a__ctor@hotmail.com>
2016-04-05 20:18:13 +0200

    Documented the SimpleFile class.

--- OfcCore/SimpleFile.cs
+++ OfcCore/SimpleFile.cs
@@ -2,22 +2,54 @@
 {
     using System.IO;
 
+    /// <summary>
+    ///     A simple implementation of the IFile interface.
+    /// </summary>
     public class SimpleFile : IFile
     {
         private FileInfo _info;
 
 
+        /// <summary>
+        ///     Returns <c>true</c> if the file exists.
+        /// </summary>
         public bool Exists => _info.Exists;
 
+        /// <summary>
+        ///     Returns the extention of the file.
+        /// </summary>
+        /// <remarks>
+        ///     The extention contains the dot (.).
+        /// </remarks>
+        /// <example>
+        ///     test.py -> .py
+        ///     test ->
+        /// </example>
         public string Extention => _info.Extension;
 
+        /// <summary>
+        ///     Returns the FileInfo object of the file.
+        /// </summary>
         internal FileInfo Info => _info;
 
+        /// <summary>
+        ///     Returns the Name of the object.
+        /// </summary>
+        /// <remarks>
+        ///     The name contains the extention.
+        /// </remarks>
         public string Name => _info.Name;
 
+        /// <summary>
+        ///     Returns the full path of the file.
+        /// </summary>
         public string Path => _info.FullName;
 
 
+        /// <summary>
+        ///     Sets up the class with the specified path.
+        /// </summary>
+        /// <param name="path">Path to the target file.</param>
         public SimpleFile(string path)
         {
             _info = new FileInfo(path);
