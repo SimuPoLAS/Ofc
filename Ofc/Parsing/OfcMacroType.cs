efb812a32a443ae1745cef1f5ed1260d96ec5c62
Patrick Widauer <a__ctor@hotmail.com>
2016-04-07 22:14:37 +0200

    Documented the OfcMacroType class.

--- Ofc/Parsing/OfcMacroType.cs
+++ Ofc/Parsing/OfcMacroType.cs
@@ -1,9 +1,26 @@
 ﻿namespace Ofc.Parsing
 {
+    /// <summary>
+    ///     Represents all types of directive.
+    /// </summary>
     internal enum OfcMacroType
     {
+        /// <summary>
+        ///     Represents the include directive.
+        /// </summary>
+        /// <remarks>
+        ///     #include "file2"
+        /// </remarks>
         Include,
+
+        /// <summary>
+        ///     Represents the inputMode directive.
+        /// </summary>
         InputMode,
+
+        /// <summary>
+        ///     Represents the remove directive.
+        /// </summary>
         Remove
     }
 }
