e5e67437f534d9813be56bd8bf8f34cec7a1af84
Patrick Widauer <a__ctor@hotmail.com>
2016-04-07 22:17:12 +0200

    Documented the OfcListType class.

--- Ofc/Parsing/OfcListType.cs
+++ Ofc/Parsing/OfcListType.cs
@@ -1,10 +1,31 @@
 ﻿namespace Ofc.Parsing
 {
+    /// <summary>
+    ///     Represents a type of list.
+    /// </summary>
+    /// <remarks>
+    /// The integer value represents the amount of horizontal elements.
+    /// </remarks>
     internal enum OfcListType
     {
+        /// <summary>
+        /// Represents a list of scalar values.
+        /// </summary>
         Scalar = 1,
+
+        /// <summary>
+        /// Represents a list of vector values.
+        /// </summary>
         Vector = 3,
+
+        /// <summary>
+        /// Represents a list of tensor values.
+        /// </summary>
         Tensor = 9,
+        
+        /// <summary>
+        /// Represents a list of any value.
+        /// </summary>
         Anonymous
     }
 }
