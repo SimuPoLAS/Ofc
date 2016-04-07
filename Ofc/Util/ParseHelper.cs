b8948e12bbdf175abe89a6e3b6b48a8f2d8f34ba
Patrick Widauer <a__ctor@hotmail.com>
2016-04-07 21:53:36 +0200

    Documented the ParseHelper class.

--- Ofc/Util/ParseHelper.cs
+++ Ofc/Util/ParseHelper.cs
@@ -1,19 +1,29 @@
-﻿using Ofc.Parsing.Hooks;
-using OfcAlgorithm.Blocky.Integration;
-using OfcAlgorithm.Integration.Dummy;
-using OfcAlgorithm.Rounding;
-using OfcCore.Configurations;
-
-namespace Ofc.Util
+﻿namespace Ofc.Util
 {
     using System;
     using System.IO;
     using JetBrains.Annotations;
     using Ofc.IO;
     using Ofc.Parsing;
+    using Ofc.Parsing.Hooks;
+    using OfcAlgorithm.Blocky.Integration;
+    using OfcAlgorithm.Integration.Dummy;
+    using OfcAlgorithm.Rounding;
+    using OfcCore.Configurations;
 
+    /// <summary>
+    ///     Provides a set of method which speed up general parsing tasks.
+    /// </summary>
     internal static class ParseHelper
     {
+        /// <summary>
+        ///     Parses the specified file with the specified hook.
+        /// </summary>
+        /// <param name="target">Target file.</param>
+        /// <param name="hook">Target hook used when parsing.</param>
+        /// <remarks>
+        ///     If no hook is specified <see cref="EmptyHook{T}.Instance" /> will be use.
+        /// </remarks>
         internal static void ParseFile(string target, [CanBeNull] IParserHook<string> hook)
         {
             if (target == null) throw new ArgumentNullException(nameof(target));
@@ -21,11 +31,20 @@ namespace Ofc.Util
             using (var stream = new FileInputStream(target))
             {
                 var lexer = new OfcLexer(stream, true);
-                var parser = new OfcParser(lexer, hook);
+                var parser = new OfcParser(lexer, hook ?? EmptyHook<string>.Instance);
                 parser.Parse();
             }
         }
 
+        /// <summary>
+        ///     Parses the specified file with the specified hook and returns if the parsing was successful.
+        /// </summary>
+        /// <param name="target">Target file.</param>
+        /// <param name="hook">Target hook used when parsing.</param>
+        /// <remarks>
+        ///     If no hook is specified <see cref="EmptyHook{T}.Instance" /> will be use.
+        /// </remarks>
+        /// <returns>Returns <c>true</c> when the parsing was successful otherwise <c>false</c>.</returns>
         internal static bool TryParseFile(string target, [CanBeNull] IParserHook<string> hook)
         {
             if (target == null) throw new ArgumentNullException(nameof(target));
@@ -40,12 +59,31 @@ namespace Ofc.Util
             }
         }
 
+        /// <summary>
+        ///     Parses the specified directory with the specified hook.
+        /// </summary>
+        /// <param name="target">Target source file.</param>
+        /// <param name="hook">Target hook used by when parsing.</param>
+        /// <param name="recursive">
+        ///     If <c>true</c> all file in the directory will be compressed otherwise only top-level files will
+        ///     be compressed.
+        /// </param>
         internal static void ParseDirectory(string target, [CanBeNull] IParserHook<string> hook, bool recursive)
         {
             foreach (var file in Directory.EnumerateFiles(target, "*", recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly))
                 ParseFile(file, hook);
         }
 
+        /// <summary>
+        ///     Parses the specified directory with the specified hook and returns if the amount of files that were successful.
+        /// </summary>
+        /// <param name="target">Target source file.</param>
+        /// <param name="hook">Target hook used by when parsing.</param>
+        /// <param name="recursive">
+        ///     If <c>true</c> all file in the directory will be compressed otherwise only top-level files will
+        ///     be compressed.
+        /// </param>
+        /// <returns>The amount of files that were successfully parsed.</returns>
         internal static int TryParseDirectory(string target, [CanBeNull] IParserHook<string> hook, bool recursive)
         {
             var a = 0;
@@ -54,6 +92,8 @@ namespace Ofc.Util
                     a++;
             return a;
         }
+
+        #region TESTING ALGORITHM METHODS
 
         internal static void CompressFolderBlocky(string srcPath, string targetPath)
         {
@@ -98,12 +138,7 @@ namespace Ofc.Util
 
         internal static void CompressFileBlockyWithRounding(string srcPath, string targetPath, double min, double max, double epsilon, FileMode mode = FileMode.Create)
         {
-            var config = new SimpleConfiguration
-            {
-                ["RoundingMin"] = min,
-                ["RoundingMax"] = max,
-                ["RoundingEpsilon"] = epsilon
-            };
+            var config = new SimpleConfiguration {["RoundingMin"] = min, ["RoundingMax"] = max, ["RoundingEpsilon"] = epsilon};
 
             using (var str = new FileStream(targetPath, mode))
             {
@@ -131,5 +166,7 @@ namespace Ofc.Util
                 }
             }
         }
+
+#endregion
     }
 }
