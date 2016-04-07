841d29dae53e00c275f2d0a4a0aeb2b52e79115b
Patrick Widauer <a__ctor@hotmail.com>
2016-04-07 22:11:57 +0200

    Documented the OfcToken class.

--- Ofc/Parsing/OfcToken.cs
+++ Ofc/Parsing/OfcToken.cs
@@ -2,24 +2,63 @@
 {
     using JetBrains.Annotations;
 
+    /// <summary>
+    ///     Represents a token emmited by the lexer and passed to the parser.
+    /// </summary>
     internal struct OfcToken
     {
+        /// <summary>
+        ///     The type of the token.
+        /// </summary>
         internal OfcTokenType Type;
+
+        /// <summary>
+        ///     The textual payload of the token.
+        /// </summary>
         [CanBeNull] internal string Text;
 
+
+        /// <summary>
+        ///     Column at which the token starts.
+        /// </summary>
         internal uint Column;
+
+        /// <summary>
+        ///     Length of the token in characters.
+        /// </summary>
         internal uint Length;
+
+        /// <summary>
+        ///     Line at which the token starts.
+        /// </summary>
         internal uint Line;
 
 
+        /// <summary>
+        ///     Sets up the token with the specified type the payload is asserted to be <c>null</c>.
+        /// </summary>
+        /// <param name="type">Type of the token.</param>
         public OfcToken(OfcTokenType type) : this(type, null)
         {
         }
 
+        /// <summary>
+        ///     Sets up the token with the specified type and payload.
+        /// </summary>
+        /// <param name="type">Type of the token.</param>
+        /// <param name="text">Payload of the token.</param>
         public OfcToken(OfcTokenType type, [CanBeNull] string text) : this(type, text, 0, 0, 0)
         {
         }
 
+        /// <summary>
+        ///     Sets up the token with the specified type and payload as well es token positioning and sizing information.
+        /// </summary>
+        /// <param name="type">Type of the token.</param>
+        /// <param name="text">Payload of the token.</param>
+        /// <param name="column">Column at which the token starts.</param>
+        /// <param name="length">Length of the token in characters.</param>
+        /// <param name="line">Line at which the token starts.</param>
         public OfcToken(OfcTokenType type, [CanBeNull] string text, uint column, uint length, uint line)
         {
             Type = type;
